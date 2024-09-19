b96f888246851542131ec4c8e23b433ab83cb7c9
Sarwar <saifsarwar404@gmail.com>
2024-09-19 09:31:17 +0600

    Add my home page

--- js/login.js
+++ js/login.js
@@ -1,7 +1,15 @@
 document.getElementById('login-btn').addEventListener('click',function(event){
     event.preventDefault();
-    console.log('login-btn clicked');
-
+    
+    
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+    if(phoneNumber=== '5' && pinNumber=== '1234'){
+        console.log('You logged in successfully');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong phone number or pin number');
+    }
 })
