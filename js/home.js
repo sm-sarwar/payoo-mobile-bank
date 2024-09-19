c1017cf9797bb4ae3b78de33e00b182df41ed876
Sarwar <saifsarwar404@gmail.com>
2024-09-19 10:08:32 +0600

    added partially event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,6 @@
-console.log('Home added')
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('add-money-amount').value;
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    console.log(addMoneyInput, inputPinNumber) ;
+})
